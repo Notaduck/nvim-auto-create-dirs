9550acdce25e2d3ab37d82efb634ad569f1718c1
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-12-16 22:08:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# auto-create-dirs.nvim
+
+This Neovim plugin automatically creates parent directories when saving a file, preventing errors when the directory does not exist.
+
+## Installation
+
+Use [Lazy.nvim](https://github.com/folke/lazy.nvim) to install:
+
+```lua
+{
+  "your-github-username/auto-create-dirs.nvim",
+  config = true,
+}
---
+++ lua/nvim-auto-create-dirs/init.lua
@@ -0,0 +1,28 @@
+-- File: lua/auto_create_dirs/init.lua
+local M = {}
+
+function M.setup()
+  -- Automatically create directories when opening a file
+  vim.api.nvim_create_autocmd("BufNewFile", {
+    pattern = "*",
+    callback = function()
+      local file_path = vim.fn.expand("<afile>:p:h")
+      if vim.fn.isdirectory(file_path) == 0 then
+        vim.fn.mkdir(file_path, "p")
+      end
+    end,
+  })
+
+  -- Automatically create directories before saving a file
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+      local file_path = vim.fn.expand("<afile>:p:h")
+      if vim.fn.isdirectory(file_path) == 0 then
+        vim.fn.mkdir(file_path, "p")
+      end
+    end,
+  })
+end
+
+return M
