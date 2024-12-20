e06404076cdb706f808f9550a3ac400ec88c80c5
Daniel Guldberg Aaes <daniel.aaes@indigounderwriters.com>
2024-12-20 20:28:59 +0100

    feat: auto-initialize setup in auto-create-dirs plugin
    Automatically call the `setup()` function when the plugin is required, eliminating the need for explicit configuration in the plugin manager.

--- lua/nvim-auto-create-dirs/init.lua
+++ lua/nvim-auto-create-dirs/init.lua
@@ -2,27 +2,29 @@
 local M = {}
 
 function M.setup()
-  -- Automatically create directories when opening a file
-  vim.api.nvim_create_autocmd("BufNewFile", {
-    pattern = "*",
-    callback = function()
-      local file_path = vim.fn.expand("<afile>:p:h")
-      if vim.fn.isdirectory(file_path) == 0 then
-        vim.fn.mkdir(file_path, "p")
-      end
-    end,
-  })
+	-- Automatically create directories when opening a file
+	vim.api.nvim_create_autocmd("BufNewFile", {
+		pattern = "*",
+		callback = function()
+			local file_path = vim.fn.expand("<afile>:p:h")
+			if vim.fn.isdirectory(file_path) == 0 then
+				vim.fn.mkdir(file_path, "p")
+			end
+		end,
+	})
 
-  -- Automatically create directories before saving a file
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function()
-      local file_path = vim.fn.expand("<afile>:p:h")
-      if vim.fn.isdirectory(file_path) == 0 then
-        vim.fn.mkdir(file_path, "p")
-      end
-    end,
-  })
+	-- Automatically create directories before saving a file
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*",
+		callback = function()
+			local file_path = vim.fn.expand("<afile>:p:h")
+			if vim.fn.isdirectory(file_path) == 0 then
+				vim.fn.mkdir(file_path, "p")
+			end
+		end,
+	})
 end
+
+M.setup()
 
 return M
